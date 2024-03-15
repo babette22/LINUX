c0158ae31bc9ce22c2cb485cf2e543d6900e9734
devopseasylearning <info@devopseasylearning.com>
2024-03-15 06:34:09 -0400

    deletion

--- teraform-project24/s3-replication-babette/s3-bucket-replication/iam-policy.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_policy" "replication" {
-  name   = format("%s-aws_iam-policy", var.common_tags["id"])
-  policy = data.aws_iam_policy_document.replication.json
-}
--- teraform-project24/s3-replication-babette/s3-bucket-replication/terraform.tfvars
+++
@@ -1,2 +0,0 @@
-aws_region_main   = "us-east-1"
-aws_region_backup = "us-east-2"
